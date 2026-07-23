1526d5027ded672e519524a4d4d0ac34eb77987e
marcusneuhaus76-create <marcusneuhaus76@gmail.com>
2026-07-23 14:07:33 +0200

    Icons left Sidebar

---
+++ Alert-Privacy-Policy.js
@@ -0,0 +1,9 @@
+// Einfache Nachricht
+alert("Please accept Up the Privacy Policy");
+
+
+
+// Bei einem Event, z.B. Button-Klick
+document.getElementById("meinButton").addEventListener("click", () => {
+  alert("Button wurde geklickt!");
+});
