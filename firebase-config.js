e7b2168a2abb62bb582284ebd86622e9a5b1bfce
NickBalzer-mConnect <nb@m-cnc.de>
2026-08-05 11:49:33 +0200

    test

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
 // Firebase Realtime Database - Basis-URL
 // TODO: Platzhalter durch eure URL aus der Firebase Console ersetzen.
 // Sie endet auf ".firebasedatabase.app" (steht oben in der Realtime Database).
+
+//Denis
 //const BASE_URL = "https://join-3222-default-rtdb.europe-west1.firebasedatabase.app/";
+
+//Nick
 const BASE_URL = "https://join-fe7b4-default-rtdb.europe-west1.firebasedatabase.app/";
